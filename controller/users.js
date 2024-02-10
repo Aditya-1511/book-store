30cd23fef05868717802c718820d4159a893f524
Aditya Pandey <aditya1511ge@gmail.com>
2024-02-10 23:31:29 +0530

    Added signup API for author

--- controller/users.js
+++ controller/users.js
@@ -36,4 +36,37 @@ router.post("/retail-user/signup", async (req, res) => {
   }
 });
 
+router.post("/author/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      throw new Error("User already exist.");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: hashedPassword,
+      userType: 3,
+    });
+
+    await newUser.save();
+
+    const respObj = {
+      message: "User created successfully",
+      data: newUser,
+    };
+
+    res.send(respObj);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send("Error creating user");
+  }
+});
+
 module.exports = router;
